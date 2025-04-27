2ff97d4d457a739940e7023280e9280571b4b3c9
romainaugier <augier.romain@sfr.fr>
2025-04-27 21:28:58 +0200

    modified:   src/symtable.c 	modified:   libromano (modified content, untracked content)

--- src/symtable.c
+++ src/symtable.c
@@ -61,5 +61,31 @@ void v_sym_debug(VSym* sym)
 
 void v_symtable_debug(VSymTable* symtable)
 {
-    VENOM_NOT_IMPLEMENTED;
+    HashMapIterator it = 0;
+
+    void* key = NULL;
+    uint32_t key_size = 0;
+    void* value = NULL;
+    uint32_t value_size = 0;
+
+    while(hashmap_iterate(symtable->symbols,
+                          &it,
+                          &key,
+                          &key_size,
+                          &value,
+                          &value_size))
+    {
+        if(key == NULL || value == NULL)
+        {
+            continue;
+        }
+
+        String* sym_name = (String*)key;
+        VSym* sym = (VSym*)key;
+
+        printf("Symbol: %s\n", sym_name);
+        printf("  ");
+
+        v_sym_debug(sym);
+    }
 }
